a9e7af394b8eded401ab818f1d2c6c30d8263dd2
Matt Castillo <hi@mattcastillo.com>
2020-09-27 15:11:19 -0700

    Try this

--- sodastream.py
+++ sodastream.py
@@ -24,6 +24,12 @@ for food in foods:
             outOfStock = True
 
 if outOfStock == True:
+    conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
+    conn.ehlo() # call this to start the connection
+    conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
+    conn.login('mattcastillo2010@gmail.com', 'xghumulesnhngkyf')
+    conn.sendmail('mattcastillo2010@gmail.com', 'mattcastillo2010@gmail.com', 'Subject: SodaStream Out of Stock\n\nStill out of stock :(\n\n')
+    conn.quit()
     print('SodaStream is not available.')
 else:
     conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
