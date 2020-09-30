e0491d17c54d0bcc142134d5e35a81e9893b02c7
Matt Castillo <hi@mattcastillo.com>
2020-09-30 07:33:20 -0700

    Fixed it after debugging the sodastream live site with 'add to basket'

--- sodastream.py
+++ sodastream.py
@@ -11,27 +11,19 @@ getPage.raise_for_status() #if error it will stop the program
 
 #Parse text for foods
 menu = bs4.BeautifulSoup(getPage.text, 'html.parser')
-foods = menu.select('.btn-add')
+button = menu.select('.btn-add')
 
-the_one = 'out of stock' # If the button says 'Out of Stock'
+the_one = '- add to basket' # If the button says 'Out of Stock'
 flength = len(the_one)
-outOfStock = True
+inStock = False
 
-for food in foods:
-    for i in range(len(food.text)):
-        chunk = food.text[i:i+flength].lower()
+for action in button:
+    for i in range(len(action.text)):
+        chunk = action.text[i:i+flength].lower()
         if chunk == the_one:
-            outOfStock = True
+            inStock = True
 
-if outOfStock == True:
-    conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
-    conn.ehlo() # call this to start the connection
-    conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
-    conn.login('mattcastillo2010@gmail.com', 'xghumulesnhngkyf')
-    conn.sendmail('mattcastillo2010@gmail.com', 'mattcastillo2010@gmail.com', 'Subject: SodaStream Out of Stock\n\nStill out of stock :(\n\n')
-    conn.quit()
-    print('SodaStream is not available.')
-else:
+if inStock == True:
     conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
     conn.ehlo() # call this to start the connection
     conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
@@ -42,3 +34,12 @@ else:
     for i in range(len(toAddress)):
         print(toAddress[i])
     print('')
+else:
+    conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
+    conn.ehlo() # call this to start the connection
+    conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
+    conn.login('mattcastillo2010@gmail.com', 'xghumulesnhngkyf')
+    conn.sendmail('mattcastillo2010@gmail.com', 'mattcastillo2010@gmail.com', 'Subject: SodaStream Out of Stock\n\nStill out of stock :(\n\n')
+    conn.quit()
+    print('SodaStream is not available.')
+    
